1024eb9145605d9e473363c7fe5cba796154f88c
Prasanth <g.prasanth1000@gmail.com>
2020-08-11 12:46:53 +0530

    CRUD operations using APIView class

--- project11/app11/views.py
+++ project11/app11/views.py
@@ -28,24 +28,3 @@ class ProductOperations(APIView):
             message = {'error':ps.errors}
         return Response(message)
 
-    def delete(self,request,product_no=None):
-        try:
-            ProductModel.objects.get(pno=product_no).delete()
-            message = {'message':'product deleted successfully'}
-        except ProductModel.DoesNotExist as de:
-            message = {'error':'Dous not exist'}
-        return Response(message)
-
-    def put(self,request,product_no):
-        try:
-            opd = ProductModel.objects.get(pno=product_no)
-            ps = ProductSerializer(data=request.data,instance=opd)
-            if ps.is_valid():
-                ps.save()
-                message = {'message': 'Product Updated Successfully'}
-            else:
-                message = {'error': ps.errors}
-        except ProductModel.DoesNotExist as de:
-            message = {'error':'Dous not exist'}
-        return Response(message)
-
